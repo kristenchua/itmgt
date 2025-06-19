8e0c197bb271dcb0f44808c9d60a13c3400f4602
kristenchua <kristen.mikayla.chua@student.ateneo.edu>
2025-06-19 20:32:43 +0800

    Update set_1.py

--- set_1.py
+++ set_1.py
@@ -1,1 +1,21 @@
+'''
+Savings
+'''
+def savings(gross_pay, tax_rate, expenses):
+  net_income=int(gross_pay*(1-tax_rate))
+  return net_income-expenses
 
+'''
+Material Waste
+'''
+def material_waste(total_material, material_units, num_jobs, job_consumption):
+  material_used=num_jobs*job_consumption
+  material_left=total_material-material_used
+  return str(material_left)+material_units
+
+'''
+Interest
+'''
+def interest(principal, rate, periods):
+  total=int(principal*rate*periods)
+  return principal+total
